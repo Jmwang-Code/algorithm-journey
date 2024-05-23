9802d62b7f42eda2bbfcf3353066ce1f1f5d9d42
79283 <792836909@qq.com>
2024-05-23 18:01:54 +0800

    算法讲解003二进制和位运算.java  算法讲解002从社会实验到入门提醒.java

---
+++ lib/入门/class003位运算/题型/灵活使用与和异或.java
@@ -0,0 +1,57 @@
+package 入门.class003位运算.题型;
+
+import static 入门.class003位运算.题型.灵活使用与和异或.Leetcode137.singleNumber;
+
+public class 灵活使用与和异或 {
+
+    public static void main(String[] args) {
+        int[] arr = {2, 2, 3, 2};
+        System.out.println(singleNumber(arr));
+    }
+
+
+    static class Leetcode136 {
+        public static int singleNumber(int[] nums) {
+            int ans = 0;
+            for (int num : nums) {
+                ans ^= num;
+            }
+            return ans;
+        }
+    }
+
+    static class Leetcode137 {
+        public static int singleNumber(int[] nums) {
+            int ans = 0;
+            for (int i = 0; i < 32; i++) {
+                int count = 0;
+                for (int num : nums) {
+                    count += (num >> i) & 1;
+                }
+                if (count % 3 != 0) {
+                    ans |= 1 << i;
+                }
+            }
+            return ans;
+        }
+    }
+
+    static class Leetcode260 {
+        public int[] singleNumber(int[] nums) {
+            int xor = 0;
+            for (int num : nums) {
+                xor ^= num;
+            }
+            int mask = xor & (-xor);
+            int a = 0, b = 0;
+            for (int num : nums) {
+                if ((num & mask) == 0) {
+                    a ^= num;
+                } else {
+                    b ^= num;
+                }
+            }
+            return new int[]{a, b};
+        }
+    }
+}
