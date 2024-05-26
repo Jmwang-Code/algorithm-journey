a2fe9262aa01e7170e35885bbfd4c20acc31b2c0
79283 <792836909@qq.com>
2024-05-26 20:13:48 +0800

    算法讲解003二进制和位运算.java  算法讲解002从社会实验到入门提醒.java

--- lib/入门/class003位运算/题型/灵活使用与和异或.java
+++ lib/入门/class003位运算/题型/灵活使用与和异或.java
@@ -9,7 +9,6 @@ public class 灵活使用与和异或 {
         System.out.println(singleNumber(arr));
     }
 
-
     static class Leetcode136 {
         public static int singleNumber(int[] nums) {
             int ans = 0;
@@ -20,6 +19,9 @@ public class 灵活使用与和异或 {
         }
     }
 
+    /**
+     * 纵向对比
+     */
     static class Leetcode137 {
         public static int singleNumber(int[] nums) {
             int ans = 0;
@@ -36,6 +38,9 @@ public class 灵活使用与和异或 {
         }
     }
 
+    /**
+     * 同一位 分成0和1 两队
+     */
     static class Leetcode260 {
         public int[] singleNumber(int[] nums) {
             int xor = 0;
@@ -65,6 +70,53 @@ public class 灵活使用与和异或 {
                 }
             }
             return count;
+        }
+    }
+
+    /**
+     * 纵向对比
+     */
+    static class leetcode476{
+        //num
+        //0101 5
+
+        //k
+        //0001  第一次 1
+        //0011  第二次 3
+        //0111  第三次 7
+
+        //k ^ num
+        //0101 0111
+        //0010 2
+        public int findComplement(int num) {
+            int k = 0;
+            while(k < num){
+                k = (k << 1) | 1;
+            }
+            return k ^ num;
+        }
+
+        //num
+        //0101 5
+
+        //n
+        //0101 5
+        //0010 2  0111 7     1
+        //0001 3  0111 7     2
+        //0000 7  0111 7     4
+        //0000 7  0111 7     8
+        //0000 7  0111 7     16
+
+        //0111 ^ 0101
+        //0010 2
+        public int findComplement2(int num) {
+            int n = num;
+            n |= n >>> 1;
+            n |= n >>> 2;
+            n |= n >>> 4;
+            n |= n >>> 8;
+            n |= n >>> 16;
+            return n ^ num;
         }
     }
 }
